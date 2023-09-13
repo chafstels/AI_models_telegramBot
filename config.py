34383a2ada1058eb067ec4081d653ca08bbeb135
anton <antonchzhu@gmail.com>
2023-09-13 17:13:57 +0600

    tele-bot

---
+++ config.py
@@ -0,0 +1,2 @@
+bot_token = "6417208671:AAGi7aptuPQ6R7TjN6YAdr-CzbCrWyylPK8"
+api_key = ""
---
+++ message_templates.py
@@ -0,0 +1,38 @@
+message_templates = {
+    "en": {
+        "start": "Hello, I'm bot powered on API GPT-4(ChatGPT). Enter /help",
+        "new_topic": "Starting a new topic!",
+        "image_prompt": "Please add a description of the image after the /image command. For example, /image Neon City.",
+        "image_error": "An error occurred during image generation:",
+        "about": "This bot is powered by OpenAI GPT-4.",
+        "help": "You can interact with this bot using commands like\n"
+        "/start\n"
+        "/newtopic'\n"
+        "/image\n"
+        "/about\n"
+        "/help\n",
+        "language_confirmation": "Language has been set to English.",
+        "language_selection": "Available in 3 languages:",
+        "processing": "Your request is being processed, please wait.",
+        "error": "An error occurred during processing:",
+    },
+    "ru": {
+        "start": "Привет, я бот, работающий на API GPT-4(ChatGPT). Введите /help",
+        "new_topic": "Начинаем новую тему!",
+        "image_prompt": "Пожалуйста, добавьте описание изображения после команды /image. "
+        "Например, /image Неоновый город.",
+        "image_error": "Произошла ошибка при генерации изображения:",
+        "about": "Этот бот работает на OpenAI GPT-4.",
+        "help": "Вы можете взаимодействовать с этим ботом с помощью команд вроде\n "
+        "/start\n"
+        "/newtopic'\n"
+        "/image\n"
+        "/about\n"
+        "/help\n"
+        "/language",
+        "language_confirmation": "Язык был изменен на русский.",
+        "language_selection": "Доступно на 3 языках:",
+        "processing": "Ваш запрос обрабатывается, пожалуйста, подождите.",
+        "error": "Произошла ошибка во время обработки:",
+    },
+}
