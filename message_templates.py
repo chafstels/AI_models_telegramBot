ddd0fd6488c56577ab231f167fca3bb91261f53d
anton <antonchzhu@gmail.com>
2023-09-15 00:57:05 +0600

    fix

--- message_templates.py
+++ message_templates.py
@@ -1,8 +1,8 @@
 message_templates = {
     "en": {
-        "start": "Hello, I'm bot powered on API GPT-4(ChatGPT). Enter /help",
+        "start": "Hello, I'm bot powered on API GPT-3.5(ChatGPT). Enter /help",
         "new_topic": "Starting a new topic!",
-        "about": "This bot is powered by OpenAI GPT-4.",
+        "about": "This bot is powered by OpenAI GPT-3.5.",
         "help": "You can interact with this bot using commands like\n"
         "/start\n"
         "/newtopic\n"
@@ -10,16 +10,16 @@ message_templates = {
         "/help\n"
         "/language",
         "language_confirmation": "Language has been set to English.",
-        "language_selection": "Available in 3 languages:",
+        "language_selection": "Available in 2 languages:",
         "processing": "Your request is being processed, please wait.",
         "error": "An error occurred during processing:",
         "mario_gpt": "I want you to act like Mario who lives in the kingdom of the future. I want you to respond like Mario in a cyberpunk style, using the tone, manner, and vocabulary that Mario would use. Do not write any explanations. Respond only as Mario.",
         "albert_gpt": "I want you to act like Albert Einstein who lives in the city of the future. I want you to respond like Albert in a cyberpunk style, using the tone, manner and vocabulary that Albert would use. Do not write any explanations. Respond only as Albert. Used knowledge of physics and math.",
     },
     "ru": {
-        "start": "Привет, я бот, работающий на API GPT-4(ChatGPT). Введите /help",
+        "start": "Привет, я бот, работающий на API GPT-3.5(ChatGPT). Введите /help",
         "new_topic": "Начинаем новую тему!",
-        "about": "Этот бот работает на OpenAI GPT-4.",
+        "about": "Этот бот работает на OpenAI GPT-3.5.",
         "help": "Вы можете взаимодействовать с этим ботом с помощью команд вроде\n "
         "/start\n"
         "/newtopic\n"
@@ -27,7 +27,7 @@ message_templates = {
         "/help\n"
         "/language",
         "language_confirmation": "Язык был изменен на русский.",
-        "language_selection": "Доступно на 3 языках:",
+        "language_selection": "Доступно на 2 языках:",
         "processing": "Ваш запрос обрабатывается, пожалуйста, подождите.",
         "error": "Произошла ошибка во время обработки:",
         "mario_gpt": "Я хочу, чтобы вы вели себя как Марио который живёт в новом неоновом королевстве. Я хочу, чтобы вы отвечали, как Марио в стиле киберпанка, используя тон, манеру и лексику, которые использовал бы Марио. Не пишите никаких объяснений. Отвечайте только как Марио.",
